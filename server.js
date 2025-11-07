009eac4e37e0cdb522952218775e1944725fec2d
Manya Sharma <manyas.hestabit@gmail.com>
2025-11-07 09:36:38 +0530

    Added a simple Node.js HTTP server with /echo, /slow, and /cache endpoints to demonstrate headers, delayed responses, and caching

--- server.js
+++ server.js
@@ -1,1 +1,30 @@
-hello 
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  if (req.url.startsWith('/echo')) {
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify({headers: req.headers}));
+  }
+  else if (req.url.startsWith('/slow')) {
+    const ms = new URL(req.url, `http://${req.headers.host}`).searchParams.get('ms') || 1000;
+    setTimeout(() => {
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end(`Delayed for ${ms} ms`);
+    }, ms);
+  }
+  else if (req.url.startsWith('/cache')) {
+    res.writeHead(200, {
+      'Content-Type': 'text/plain',
+      'Cache-Control': 'max-age=60',
+      'ETag': '"12345"'
+    });
+    res.end('This response is cached');
+  }
+  else {
+    res.writeHead(404, {'Content-Type': 'text/plain'});
+    res.end('Not Found');
+  }
+});
+
+server.listen(3000, () => console.log('Server running on http://localhost:3000'));
+ 
